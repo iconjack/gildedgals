6db92ccbca7ac91a2a105eec2e497afd25a7fd06
Jack Kennedy <jack@realmode.com>
2023-04-13 11:14:03 -0500

    changed image and text

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        prompt = request.form["prompt"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(prompt),
             temperature=0.6,
         )
         print(response)
         return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    else:
+        result = request.args.get("result")
+        return render_template("index.html", result=result)
 
 
 def generate_prompt(animal):
